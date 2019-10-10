8c9cd54ee3f0e035c868f069c8dae46136389bae
Евгений Бризинский <hellabanana@mail.ru>
2019-10-10 20:04:56 +0300

    v2

---
+++ Test/Model1.edmx.sql
@@ -0,0 +1,119 @@
+
+-- --------------------------------------------------
+-- Entity Designer DDL Script for SQL Server 2005, 2008, 2012 and Azure
+-- --------------------------------------------------
+-- Date Created: 10/10/2019 19:51:25
+-- Generated from EDMX file: A:\Учеба\C#\Projects\Test\Test\Model1.edmx
+-- --------------------------------------------------
+
+SET QUOTED_IDENTIFIER OFF;
+GO
+USE [books];
+GO
+IF SCHEMA_ID(N'dbo') IS NULL EXECUTE(N'CREATE SCHEMA [dbo]');
+GO
+
+-- --------------------------------------------------
+-- Dropping existing FOREIGN KEY constraints
+-- --------------------------------------------------
+
+IF OBJECT_ID(N'[dbo].[FK_BooksEntity2]', 'F') IS NOT NULL
+    ALTER TABLE [dbo].[BookAuthorSet] DROP CONSTRAINT [FK_BooksEntity2];
+GO
+IF OBJECT_ID(N'[dbo].[FK_Entity3Entity2]', 'F') IS NOT NULL
+    ALTER TABLE [dbo].[BookAuthorSet] DROP CONSTRAINT [FK_Entity3Entity2];
+GO
+
+-- --------------------------------------------------
+-- Dropping existing tables
+-- --------------------------------------------------
+
+IF OBJECT_ID(N'[dbo].[BooksSet]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[BooksSet];
+GO
+IF OBJECT_ID(N'[dbo].[BookAuthorSet]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[BookAuthorSet];
+GO
+IF OBJECT_ID(N'[dbo].[AuthorSet]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[AuthorSet];
+GO
+
+-- --------------------------------------------------
+-- Creating all tables
+-- --------------------------------------------------
+
+-- Creating table 'BooksSet'
+CREATE TABLE [dbo].[BooksSet] (
+    [Id_book] int IDENTITY(1,1) NOT NULL,
+    [Name] nvarchar(max)  NOT NULL,
+    [Year] int  NOT NULL
+);
+GO
+
+-- Creating table 'BookAuthorSet'
+CREATE TABLE [dbo].[BookAuthorSet] (
+    [Id_book] int IDENTITY(1,1) NOT NULL,
+    [Id_author] int IDENTITY(1,1) NOT NULL
+);
+GO
+
+-- Creating table 'AuthorSet'
+CREATE TABLE [dbo].[AuthorSet] (
+    [Id_author] int IDENTITY(1,1) NOT NULL,
+    [Name] nvarchar(max)  NOT NULL
+);
+GO
+
+-- --------------------------------------------------
+-- Creating all PRIMARY KEY constraints
+-- --------------------------------------------------
+
+-- Creating primary key on [Id_book] in table 'BooksSet'
+ALTER TABLE [dbo].[BooksSet]
+ADD CONSTRAINT [PK_BooksSet]
+    PRIMARY KEY CLUSTERED ([Id_book] ASC);
+GO
+
+-- Creating primary key on [Id_book], [Id_author] in table 'BookAuthorSet'
+ALTER TABLE [dbo].[BookAuthorSet]
+ADD CONSTRAINT [PK_BookAuthorSet]
+    PRIMARY KEY CLUSTERED ([Id_book], [Id_author] ASC);
+GO
+
+-- Creating primary key on [Id_author] in table 'AuthorSet'
+ALTER TABLE [dbo].[AuthorSet]
+ADD CONSTRAINT [PK_AuthorSet]
+    PRIMARY KEY CLUSTERED ([Id_author] ASC);
+GO
+
+-- --------------------------------------------------
+-- Creating all FOREIGN KEY constraints
+-- --------------------------------------------------
+
+-- Creating foreign key on [Id_book] in table 'BookAuthorSet'
+ALTER TABLE [dbo].[BookAuthorSet]
+ADD CONSTRAINT [FK_BooksEntity2]
+    FOREIGN KEY ([Id_book])
+    REFERENCES [dbo].[BooksSet]
+        ([Id_book])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating foreign key on [Id_author] in table 'BookAuthorSet'
+ALTER TABLE [dbo].[BookAuthorSet]
+ADD CONSTRAINT [FK_Entity3Entity2]
+    FOREIGN KEY ([Id_author])
+    REFERENCES [dbo].[AuthorSet]
+        ([Id_author])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_Entity3Entity2'
+CREATE INDEX [IX_FK_Entity3Entity2]
+ON [dbo].[BookAuthorSet]
+    ([Id_author]);
+GO
+
+-- --------------------------------------------------
+-- Script has ended
+-- --------------------------------------------------
--- Test/Program.cs
+++ Test/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Core.EntityClient;
+using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +11,174 @@ namespace Test
 {
     class Program
     {
+        private static Model1Container1 connection { get; set; }
+
         static void Main(string[] args)
         {
+           // EntityConnectionStringBuilder entityConnectionStringBuilder = new EntityConnectionStringBuilder();
+           // entityConnectionStringBuilder.ConnectionString = $"metadata = res://*/Model1.csdl|res://*/Model1.ssdl|res://*/Model1.msl;Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename={Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "books.mdf")};Integrated Security=True;Connect Timeout=30";
+            connection = new Model1Container1();
+        
+          
+            while (true)
+            {
+                Console.WriteLine("1.Добавить книгу");
+                Console.WriteLine("2.Добавить автора");
+                Console.WriteLine("3.Вывести книги");
+                Console.WriteLine("4.Вывести авторов");
+                Console.WriteLine("5.Поиск по автору");
+                Console.WriteLine("6.Удаление книги");
+                if (int.TryParse(Console.ReadLine(), out int i))
+                {
 
+                    switch (i)
+                    {
+                        case 1: { Console.Clear(); BookAdd(); break; }
+                        case 2: { Console.Clear(); AuthorAdd(); break; }
+                        case 3: { Console.Clear(); ShowBooks(); break; }
+                        case 4: { Console.Clear(); ShowAuthor(); break; }
+                        case 5: { Console.Clear(); SearchByAuthor(); break; }
+                        case 6: { Console.Clear(); DeleteBook(); break; }
+
+                    }
+                }
+                else Console.WriteLine("Неверный выбор");
+            }
         }
+
+        private static void BookAdd()
+        {
+            Console.WriteLine("Введите имя книги:");
+            string _nm = Console.ReadLine();
+            Console.WriteLine("Введите год:");
+            if (int.TryParse(Console.ReadLine(), out int _year))
+            { 
+                connection.BooksSet.Add(new Books() { Name = _nm, Year = _year });
+                connection.SaveChanges();
+                Console.WriteLine("Введите номера авторов через запятую");
+                foreach (Author item in connection.AuthorSet)
+                {
+                    Console.WriteLine(item.Id_author + "." + item.Name);
+                }
+
+                string[] aut = Console.ReadLine().Split(',');
+
+                foreach (string item in aut)
+                {
+                    int bokId = connection.BooksSet.Select(x => x.Id_book).Max();
+                    connection.BookAuthorSet.Add(new BookAuthor() { Id_author = int.Parse(item), Id_book = bokId });
+
+                }
+                connection.SaveChanges();
+            }
+            else Console.WriteLine("Введено неверное значение");
+        }
+
+        private static void DeleteBook()
+        {
+            Console.WriteLine("Введите номер книги для удаления");
+           
+                foreach (var item in connection.BooksSet)
+                {
+                Console.WriteLine(item.Id_book+"."+item.Name);
+
+                }
+            Console.WriteLine();
+            if (int.TryParse(Console.ReadLine(), out int _delid))
+            {
+                connection.BooksSet.Remove(connection.BooksSet.Find(_delid));
+                connection.SaveChanges();
+            }
+            else Console.WriteLine("Неверное значение");
+        }
+
+        private static void SearchByAuthor()
+        {
+            if (connection.AuthorSet.Count() != 0)
+            {
+                foreach (var item in connection.AuthorSet)
+                {
+                    Console.WriteLine(item.Id_author + "." + item.Name);
+
+                }
+                Console.WriteLine();
+                Console.WriteLine("Выберите номер автора для поиска:");
+                if (int.TryParse(Console.ReadLine(), out int _idAuthor))
+                {
+
+                    var res = connection.BookAuthorSet.Where(x => x.Id_author == _idAuthor).Select(f => f.Id_book);
+                    var names = connection.BooksSet.Join(res, // второй набор
+        p => p.Id_book, // свойство-селектор объекта из первого набора
+        c => c, // свойство-селектор объекта из второго набора
+        (p, c) => new // результат
+        {
+            Name = p.Name
+
+        });
+
+                    foreach (var authorEntity in names)
+                    {
+                        Console.WriteLine(authorEntity.Name);
+                    }
+
+                }
+                else Console.WriteLine("Записей нет!");
+            }
+        }
+
+        private static void ShowAuthor()
+        {
+            if (connection.AuthorSet.Count() != 0)
+            {
+                foreach (var item in connection.AuthorSet)
+                {
+                    Console.WriteLine(item.Id_author + "." + item.Name); //A:\УЧЕБА\C#\PROJECTS\TEST\TEST\
+
+                }
+                Console.WriteLine();
+            }
+            else Console.WriteLine("Записей нет!");
+            Console.WriteLine();
+        }
+
+        private static void ShowBooks()
+        {
+            if (connection.BooksSet.Count() != 0)
+            {
+                var result = from BookAuthor in connection.BookAuthorSet
+                             join Author in connection.AuthorSet on BookAuthor.Id_author equals Author.Id_author
+                             select new
+                             {
+                                 Name = Author.Name,
+                                 Id_book = BookAuthor.Id_book,
+
+                             };
+
+
+                foreach (var item in connection.BooksSet)
+                {
+                    string s = item.Id_book + "." + item.Name + " " + item.Year;
+                    foreach (string z in result.Where(x => x.Id_book == item.Id_book).Select(f => f.Name))
+                    {
+                        s += " " + z;
+
+                    }
+
+                    Console.WriteLine(s);
+
+                }
+            }
+            else Console.WriteLine("Записей нет!");
+            Console.WriteLine();
+        }
+
+        private static void AuthorAdd()
+        {
+            Console.WriteLine("Введите имя автора:");
+            connection.AuthorSet.Add(new Author() { Name = Console.ReadLine() });
+            connection.SaveChanges();
+            Console.WriteLine();
+        }
+
     }
 }
